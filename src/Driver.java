ece3d96d3fa63dd99cf1d271e5e5f117b22910be
Ahmad Khateeb <ahmadkhateebq@gmail.com>
2023-02-06 23:10:13 +0200

    Update Driver.java
    added a comment

--- src/Driver.java
+++ src/Driver.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Driver {
     public static void main(String[] args) throws ParseException, FileNotFoundException {
+        //the path to your file that you want to patse 
         String URL = "C:\\Users\\Ahmad Khateeb\\Downloads\\whatever.txt";
         File file = new File (URL);
         Scanner scanner = new Scanner (file);
